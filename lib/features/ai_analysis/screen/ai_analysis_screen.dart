7ab54a82a86f846687035f8859be00e5482eec75
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2026-05-21 12:17:48 +0600

    feat: add shimmer loading state to AI analysis screen

--- lib/features/ai_analysis/screen/ai_analysis_screen.dart
+++ lib/features/ai_analysis/screen/ai_analysis_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:saifsyn/features/ai_analysis/controller/ai_analysis_controller.dart';
 import 'package:saifsyn/features/ai_analysis/widgets/ai_analysis_screen_widgets.dart';
+import 'package:shimmer/shimmer.dart';
 
 class AiAnalysisScreen extends StatelessWidget {
   const AiAnalysisScreen({super.key});
@@ -34,29 +35,31 @@ class AiAnalysisScreen extends StatelessWidget {
                     ),
                   )),
               Expanded(
-                child: controller.isBootstrapping && !controller.hasResult
-                    ? const Center(child: CircularProgressIndicator())
-                    : RefreshIndicator(
-                        onRefresh: controller.refreshCurrent,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: SingleChildScrollView(
-                                controller: controller.contentScrollController,
-                                physics: const AlwaysScrollableScrollPhysics(),
-                                padding: EdgeInsets.fromLTRB(
-                                  isMobile ? 12.w : 16.w,
-                                  isMobile ? 12.h : 18.h,
-                                  showSidebar ? 8.w : (isMobile ? 12.w : 16.w),
-                                  isMobile ? 18.h : 28.h,
-                                ),
-                                child: AiAnalysisMainContent(
+                child: RefreshIndicator(
+                  onRefresh: controller.refreshCurrent,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: SingleChildScrollView(
+                          controller: controller.contentScrollController,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          padding: EdgeInsets.fromLTRB(
+                            isMobile ? 12.w : 16.w,
+                            isMobile ? 12.h : 18.h,
+                            showSidebar ? 8.w : (isMobile ? 12.w : 16.w),
+                            isMobile ? 18.h : 28.h,
+                          ),
+                          child: controller.isSearching ||
+                                  (controller.isBootstrapping &&
+                                      !controller.hasResult)
+                              ? const AiAnalysisShimmerLoading()
+                              : AiAnalysisMainContent(
                                   controller: controller,
                                   isMobile: isMobile,
                                 ),
-                              ),
-                            ),
+                        ),
+                      ),
                             if (showSidebar)
                               Padding(
                                 padding: EdgeInsets.fromLTRB(
@@ -111,4 +114,56 @@ void _showHistorySheet(
       );
     },
   );
+}
+
+class AiAnalysisShimmerLoading extends StatelessWidget {
+  const AiAnalysisShimmerLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: const Color(0xFFE4E7EC),
+      highlightColor: const Color(0xFFF2F4F7),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 100.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Container(
+            height: 140.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Container(
+            height: 350.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Container(
+            height: 200.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
